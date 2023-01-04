10b525335de2e3a224bbb4f655246c5c459a0ab0
martinromario55 <martinromario55@gmail.com>
2023-01-04 16:48:53 +0300

    sixth commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,11 +6,18 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
@@ -26,7 +33,7 @@ def post_list(request):
         # If page_number is out of range
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'posts' : posts})
+    return render(request, 'blog/post/list.html', {'posts' : posts, 'tag':tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -42,7 +49,13 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True) #Get all active comments related to the current post
     form = CommentForm() # Load an instance of the comment form
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments':comments, 'form':form})
+    # Listing similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments':comments, 'form':form, 'similar_posts':similar_posts})
 
 
 # Using aclass-based view
